c0f63ad99238239eed70f277abd08de640a00edd
Server <ubuntu@localhost>
2021-12-22 18:16:33 +0800

    :book: update by server

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-git pull
 python ./update.sh
 git config user.name "Server"
 git config user.email "ubuntu@localhost"
 git add .
 git commit -m ":book: update by server"
+git pull
+git push
